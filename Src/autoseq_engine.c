9f48a4af5ef09b253a1c5acf5f7e41b0078819eb
N6HAN <spica.han@gmail.com>
2025-06-15 20:47:52 -0700

    Fix a bug of always sending +0 as report when skipping grid

--- Src/autoseq_engine.c
+++ Src/autoseq_engine.c
@@ -107,6 +107,7 @@ void autoseq_on_touch(const Decode *msg)
     // Must be handling TX6
     strncpy(ctx.dxcall, msg->call_from, sizeof(ctx.dxcall) - 1);
     strncpy(ctx.dxgrid, msg->locator,   sizeof(ctx.dxgrid) - 1);
+    ctx.snr_tx = msg->snr;
     set_state(Skip_Tx1 ? AS_REPORT : AS_REPLYING,
               Skip_Tx1 ? TX2 : TX1, MAX_TX_RETRY);
 }
